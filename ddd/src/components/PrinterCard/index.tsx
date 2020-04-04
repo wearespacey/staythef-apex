3bf80f67692897ddf1f7d1631683222316c000df
HunteRoi <tinael.devresse@hotmail.com>
2020-04-04 18:37:43 +0200

    Fix CSS issue with button and card

--- ddd/src/components/PrinterCard/index.tsx
+++ ddd/src/components/PrinterCard/index.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import { IPrinterModel } from '../../models/IPrinterModel';
 import printerAvatar from '../../assets/printer3d.png';
+import { EntityCard } from '../EntityCard';
 
 interface IProps {
   printer: IPrinterModel;
+  width: number;
+  height: number;
 }
 
-export const PrinterCard: React.FC<IProps> = ({ printer }) => {
+export const PrinterCard: React.FC<IProps> = ({ printer, width, height }) => {
   return (
-    <Card>
-      <Card.Img variant='top' src={printerAvatar} />
-      <Card.Body>
-        <Card.Title>{printer.id}</Card.Title>
-        <Card.Text>{printer.details}</Card.Text>
-      </Card.Body>
-    </Card>
+    <EntityCard width={width} height={height}>
+        <Card.Img variant='top' src={printerAvatar} />
+        <Card.Body>
+          <Card.Title>{printer.id}</Card.Title>
+          <Card.Text>{printer.details}</Card.Text>
+        </Card.Body>
+      </EntityCard>
   );
 };

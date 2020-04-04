d1a1be8350bcb316d4d70ad1103c2188b7ff66d0
xvercruysse <vercruysse.xavier@xvercruysse.tech>
2020-04-05 00:27:35 +0200

    fghxsuht

--- backend/src/shared/firestore.ts
+++ backend/src/shared/firestore.ts
@@ -8,15 +8,16 @@ export class Firestore {
 
   params = {
     type: process.env['Values.type'],
-    projectId: process.env['Values.project_id'],
-    privateKeyId: process.env['Values.private_key_id'],
-    privateKey: process.env['Values.private_key'],
-    clientEmail: process.env['Values.client_email'],
-    clientId: process.env['Values.client_id'],
-    authUri: process.env['Values.auth_uri'],
-    tokenUri: process.env['Values.token_uri'],
-    authProviderX509CertUrl: process.env['Values.auth_provider_x509_cert_url'],
-    clientC509CertUrl: process.env['Values.client_x509_cert_url'],
+    project_id: process.env['Values.project_id'],
+    private_key_id: process.env['Values.private_key_id'],
+    private_key: process.env['Values.private_key'],
+    client_email: process.env['Values.client_email'],
+    client_id: process.env['Values.client_id'],
+    auth_uri: process.env['Values.auth_uri'],
+    token_uri: process.env['Values.token_uri'],
+    auth_provider_x509_cert_url:
+      process.env['Values.auth_provider_x509_cert_url'],
+    clientX509_cert_Url: process.env['Values.client_x509_cert_url'],
   };
 
   constructor() {

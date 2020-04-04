4319404a53217ca1e519b2272e5a81a827f1f634
Arsero <54241944+Arsero@users.noreply.github.com>
2020-04-04 21:27:36 +0200

    Update data and UI

--- ddd/src/pages/Consumer/index.tsx
+++ ddd/src/pages/Consumer/index.tsx
@@ -105,126 +105,41 @@ let initialValuesPlans: IThreeModel[] = [
   },
 ];
 
-let initialValuesPrinter: IPrinterModel[] = [
-  {
-    id: "123",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "124",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-  {
-    id: "125",
-    details: "MARQUE 3D PRINTER",
-  },
-];
+let INTIIAL_VALUE: IPrinterModel = {
+  id: "123",
+  details: "Marque Imprimante 3D",
+};
+
+let INITIAL_3DMODEL: IThreeModel = {
+  id: "123",
+  imageUrl:
+    "https://cdn.icon-icons.com/icons2/510/PNG/512/printer_icon-icons.com_50058.png",
+  title: "First model",
+  description: "Nice model bro",
+  dimensions: "15x25x53 cm",
+  gcodeUrl: "",
+};
 
 export const Consumer = () => {
   const [isPrinter, setisPrinter] = useState(false);
-  const [modelsPlans, setModelsPlans] = useState<IThreeModel[]>(initialValuesPlans);
-  const [modelsPrinter, setmodelsPrinter] = useState<IPrinterModel[]>(initialValuesPrinter);
+  const [modelsPlans, setModelsPlans] = useState<IThreeModel[]>([
+    INITIAL_3DMODEL,
+    INITIAL_3DMODEL,
+    INITIAL_3DMODEL,
+  ]);
+
+  const [modelsPrinter, setmodelsPrinter] = useState<IPrinterModel[]>([
+    INTIIAL_VALUE,
+    INTIIAL_VALUE,
+    INTIIAL_VALUE,
+    INTIIAL_VALUE,
+  ]);
+
   return (
-    <Container fluid>
+    <>
       <Row>
         <Col xs={2}>
-          <DropdownButton
+          <DropdownButton 
             id="consumer-choice"
             variant="danger"
             title="Plans / Printers"
@@ -238,35 +153,35 @@ export const Consumer = () => {
           </DropdownButton>
         </Col>
         <Col>
-          {isPrinter ? (<CardDeck className="m-5">
+          {isPrinter ? (
+            <CardDeck className="m-4">
               {modelsPrinter.map((model, index) => (
                 <PrinterCard
-                width={300}
-                height={400}
-                key={index}
-                printer={model}
-                onClick={() => console.log("You clicked on an item!")}
-                contentButton="Negotiate !"
-              />
+                  width={300}
+                  height={450}
+                  key={index}
+                  printer={model}
+                  onClick={() => console.log("You clicked on an item!")}
+                  contentButton="Negotiate !"
+                />
               ))}
             </CardDeck>
           ) : (
-            
-            <CardDeck className="m-5">
-            {modelsPlans.map((model, index) => (
-              <ProducerCard
-                width={200}
-                height={425}
-                key={index}
-                producer={model}
-                onClick={() => console.log("You clicked on an item!")}
-                contentButton="Negotiate !"
-              />
-            ))}
+            <CardDeck className="m-4">
+              {modelsPlans.map((model, index) => (
+                <ProducerCard
+                  width={300}
+                  height={485}
+                  key={index}
+                  producer={model}
+                  onClick={() => console.log("You clicked on an item!")}
+                  contentButton="Negotiate !"
+                />
+              ))}
             </CardDeck>
           )}
         </Col>
       </Row>
-    </Container>
+    </>
   );
 };

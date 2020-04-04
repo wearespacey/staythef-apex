33a1db244e8ebd58671f97a3ada8832d7fe07274
Arsero <54241944+Arsero@users.noreply.github.com>
2020-04-04 21:26:04 +0200

    Update logo NavBar

--- ddd/src/components/NavBar/index.tsx
+++ ddd/src/components/NavBar/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { XSquare } from 'react-bootstrap-icons';
+import { XSquare, InboxesFill } from 'react-bootstrap-icons';
 
 export const NavBar = () => {
   return (
     <Navbar bg='dark' variant='dark' className='justify-content-between'>
       <Navbar.Brand href=''>
-        <img
-          alt=""
-          src="/logo.svg"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{" "}
+        <InboxesFill size={25} color='#FF8E53' style={{marginRight:'20px'}} />
         Stay the fuck Apex
       </Navbar.Brand>
       <Link to='/'>

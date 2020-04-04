91b2a2d60ef693c608d5c7d5cfd6aa9cc266d095
HunteRoi <tinael.devresse@hotmail.com>
2020-04-04 20:16:20 +0200

    Use bootstrap Accordian as toggler

--- ddd/src/pages/Producer/index.tsx
+++ ddd/src/pages/Producer/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, CardDeck, Button } from 'react-bootstrap';
+import { Container, CardDeck, Button, Accordion } from 'react-bootstrap';
 
 import { IPrinterModel } from '../../models/IPrinterModel';
 import { PrinterAddForms } from '../../components/PrinterAddForms';
@@ -116,35 +116,23 @@ let initialValues: IPrinterModel[] = [
   },
 ];
 
-const style = {
-  display: 'none',
-};
-
-function showtoggle(){
-  var x = document.getElementById("form");
-  if(x != null){
-    if (x.style.display === "none") {
-    x.style.display = "block";
-    } else {
-    x.style.display = "none";
-    }
-  }
-}
-
 
 export const Producer = () => {
   const [models, setModels] = useState<IPrinterModel[]>(initialValues);
 
   return (
     <div>
-      <Button onClick={showtoggle} variant='primary' size='lg' block>
-        Ajouter une imprimante 3d
-      </Button>
-      <div id="form" style={style}>
-        <Container>
-          <PrinterAddForms />
-        </Container>
-      </div>
+      <Accordion>
+        <Accordion.Toggle as={Button} eventKey='0'>
+          Toggle form
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey='0'>
+          <Container>
+            <PrinterAddForms />
+          </Container>
+        </Accordion.Collapse>
+      </Accordion>
+
       <CardDeck className='m-5'>
         {models.map((model, index) => (
           <PrinterCard width={300} height={400} key={index} printer={model} />

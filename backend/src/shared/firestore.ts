5e56c2e77d0395e04be4af5701fa74b4f1f923d4
xvercruysse <vercruysse.xavier@xvercruysse.tech>
2020-04-05 00:20:21 +0200

    azure func

--- backend/src/shared/firestore.ts
+++ backend/src/shared/firestore.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+//import * as serviceAccount from '../../local.settings.json';
 
 @Injectable()
 export class Firestore {
   db: admin.firestore.Firestore;
 
+  params = {
+    type: process.env['Values.type'],
+    projectId: process.env['Values.project_id'],
+    privateKeyId: process.env['Values.private_key_id'],
+    privateKey: process.env['Values.private_key'],
+    clientEmail: process.env['Values.client_email'],
+    clientId: process.env['Values.client_id'],
+    authUri: process.env['Values.auth_uri'],
+    tokenUri: process.env['Values.token_uri'],
+    authProviderX509CertUrl: process.env['Values.auth_provider_x509_cert_url'],
+    clientC509CertUrl: process.env['Values.client_x509_cert_url'],
+  };
+
   constructor() {
     admin.initializeApp({
-      credential: admin.credential.cert(
-        require('../../firestore_credentials.json'),
-      ),
-      databaseURL: 'https://ddd-3ddd.firebaseio.com',
+      credential: admin.credential.cert(this.params),
     });
     this.db = admin.firestore();
   }

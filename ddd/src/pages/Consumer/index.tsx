5ecc021cfa19c4dab747796f53fe9e5fa942f090
HunteRoi <tinael.devresse@hotmail.com>
2020-04-04 17:02:52 +0200

    Fix quiproquo

--- ddd/src/pages/Consumer/index.tsx
+++ ddd/src/pages/Consumer/index.tsx
@@ -1,45 +1,5 @@
-import React, { useState } from 'react';
-import { EntityList } from '../../components/EntityList';
-import { IThreeModel } from '../../models/IThreeModel';
-import { ProducerCard } from '../../components/ProducerCard';
-
-let initialValues: IThreeModel[] = [
-  {
-    id: '123',
-    imageUrl:
-      'https://cdn.icon-icons.com/icons2/510/PNG/512/printer_icon-icons.com_50058.png',
-    title: 'First model',
-    description: 'Nice model bro',
-    dimensions: '15 cm x 25cm x 53 cm',
-    gcodeUrl: '',
-  },
-  {
-    id: '1213',
-    imageUrl:
-      'https://cdn.icon-icons.com/icons2/510/PNG/512/printer_icon-icons.com_50058.png',
-    title: 'First model',
-    description: 'Nice model bro',
-    dimensions: '15 cm x 25cm x 53 cm',
-    gcodeUrl: '',
-  },
-  {
-    id: '1223',
-    imageUrl:
-      'https://cdn.icon-icons.com/icons2/510/PNG/512/printer_icon-icons.com_50058.png',
-    title: 'First model',
-    description: 'Nice model bro',
-    dimensions: '15 cm x 25cm x 53 cm',
-    gcodeUrl: '',
-  },
-];
+import React from 'react';
 
 export const Consumer = () => {
-  const [models, setModels] = useState<IThreeModel[]>(initialValues);
-  const onClick = () => console.log('You clicked on an item!');
-
-  const children = models.map((model, index) => (
-    <ProducerCard key={index} producer={model} onClick={onClick} />
-  ));
-
-  return <EntityList>{children}</EntityList>;
+  return <span>Consumer works!</span>;
 };

a309f7b8f392cc5a5047b3fa0d84bbfd2f0ad86d
Vivien Preser <vivibike01@gmail.com>
2020-04-04 19:20:30 +0200

    toggle form

--- ddd/src/pages/Producer/index.tsx
+++ ddd/src/pages/Producer/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, CardDeck } from 'react-bootstrap';
+import { Container, CardDeck, Button } from 'react-bootstrap';
 
 import { IPrinterModel } from '../../models/IPrinterModel';
 import { PrinterAddForms } from '../../components/PrinterAddForms';
@@ -116,14 +116,35 @@ let initialValues: IPrinterModel[] = [
   },
 ];
 
+const style = {
+  display: 'none',
+};
+
+function showtoggle(){
+  var x = document.getElementById("form");
+  if(x != null){
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
+    }
+  }
+}
+
+
 export const Producer = () => {
   const [models, setModels] = useState<IPrinterModel[]>(initialValues);
 
   return (
     <div>
-      <Container>
-        <PrinterAddForms />
-      </Container>
+      <Button onClick={showtoggle} variant='primary' size='lg' block>
+        Ajouter une imprimante 3d
+      </Button>
+      <div id="form" style={style}>
+        <Container>
+          <PrinterAddForms />
+        </Container>
+      </div>
       <CardDeck className='m-5'>
         {models.map((model, index) => (
           <PrinterCard width={300} height={400} key={index} printer={model} />

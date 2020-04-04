7c189442567a944bcf25c6430863702f7e9c6ef8
Arsero <54241944+Arsero@users.noreply.github.com>
2020-04-04 20:47:22 +0200

    Update Button navbar

--- ddd/src/components/NavBar/index.tsx
+++ ddd/src/components/NavBar/index.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Navbar, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <Navbar bg='dark' variant='dark' className='justify-content-between'>
+    <Navbar bg="dark" variant="dark" className="justify-content-between">
       <Navbar.Brand href="">
         <img
-          alt=''
-          src='/logo.svg'
-          width='30'
-          height='30'
-          className='d-inline-block align-top'
-        />{' '}
+          alt=""
+          src="/logo.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{" "}
         Stay the fuck Apex
       </Navbar.Brand>
-      <Link to='/'>Back to Menu</Link>
+
+      <Link to="/">
+        <Button>Back to Menu</Button>
+      </Link>
     </Navbar>
   );
 };

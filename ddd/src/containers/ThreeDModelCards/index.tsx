80e537125cd36d330e9d0952ff41030a47553d3e
Vivien Preser <vivibike01@gmail.com>
2020-04-05 12:44:05 +0200

    get data contributor

--- ddd/src/containers/ThreeDModelCards/index.tsx
+++ ddd/src/containers/ThreeDModelCards/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CardDeck } from 'react-bootstrap';
 
 import { IThreeModel } from '../../models/IThreeModel';
@@ -15,13 +15,23 @@ const INITIAL_3DMODEL: IThreeModel = {
 };
 
 export const ThreeDModelCards = () => {
-  const [models, setModels] = useState<IThreeModel[]>([
-    INITIAL_3DMODEL,
-    INITIAL_3DMODEL,
-    INITIAL_3DMODEL,
-  ]);
+  const [models, setModels] = useState<IThreeModel[]>([]);
+
+   async function api(){
+      const res = await fetch('https://staythefuckapex-api.azurewebsites.net/api/3dmodels/all');
+      const data = await res.json();
+      setModels(data as IThreeModel[]);
+    }
+  
+    useEffect(() => {
+      api();
+      return () => {
+      }
+    }, []);
+
 
   return (
+ 
     <CardDeck className='m-4'>
       {models.map((model, index) => (
         <ThreeDModelCard

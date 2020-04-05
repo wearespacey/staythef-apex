c9cfe99352f5745a7697f296a493a793d764dc07
HunteRoi <tinael.devresse@hotmail.com>
2020-04-05 14:43:38 +0200

    bruh

--- ddd/src/components/PrinterModelForm/index.tsx
+++ ddd/src/components/PrinterModelForm/index.tsx
@@ -4,57 +4,61 @@ import { Form, Button } from 'react-bootstrap';
 export const PrinterModelForm = () => {
   const [state, setState] = useState<boolean>(true);
 
-  let handleSubmit = async (
-    e: React.FormEvent<HTMLFormElement>
-  ): Promise<void> => {
+  let handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(e);
+
     setState(false);
     if (validateForm()) {
-      const submitSuccess: boolean = await submitForm();
+      await submitForm();
     }
     setState(true);
   };
-  function validateForm(): boolean {
-    console.log("Valide form");
-    return true;
-  }
-  async function submitForm(): Promise<boolean> { // TO DO LINK with form 
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          "price": 1.2,
-          "plastic": "PLA",
-          "producerId": "hXLzwZ35hkj24iZRaPs8"
-      })
-    };
-    fetch('https://staythefuckapex-api.azurewebsites.net/api/3dprinters/new', requestOptions)
-    console.log("send");
-    return true;
-  }
 
+  function validateForm(): boolean {
+    return true;
+  }
+  async function submitForm() {
+    let submitted = false;
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        price: 1.2,
+        plastic: 'PLA',
+        producerId: 'hXLzwZ35hkj24iZRaPs8',
+      }),
+    };
+
+    fetch(
+      'https://staythefuckapex-api.azurewebsites.net/api/3dprinters/new',
+      requestOptions
+    )
+      .then(r => submitted = true)
+      .catch(e => submitted = false);
+
+    return submitted;
+  }
 
   return (
-    <Form  onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
       <Form.Group controlId='DesciptionTextField'>
         <Form.Label as='h3' className='title'>
           Plastic type
         </Form.Label>
         <Form.Control type='text' placeholder='PETG' />
       </Form.Group>
+      
       <Form.Group controlId='PriceField'>
         <Form.Label as='h3' className='title'>
           Print price
         </Form.Label>
         <Form.Control type='number' placeholder='1' />
       </Form.Group>
+      
       <Button variant='primary' type='submit'>
         Add
       </Button>
-      <div hidden={state}>
-        Loading ...
-      </div>
+      <div hidden={state}>Loading ...</div>
     </Form>
   );
 };
--- ddd/src/components/ThreeDModelForm/index.tsx
+++ ddd/src/components/ThreeDModelForm/index.tsx
@@ -1,50 +1,49 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import { SSL_OP_EPHEMERAL_RSA } from 'constants';
-import { wait } from '@testing-library/react';
-import { IThreeModel } from '../../models/IThreeModel';
-import { stringify } from 'querystring';
 
 export const ThreeDModelForm = () => {
   const [state, setState] = useState<boolean>(true);
 
-  let handleSubmit = async (
-    e: React.FormEvent<HTMLFormElement>
-  ): Promise<void> => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(e);
+
     setState(false);
     if (validateForm()) {
-      const submitSuccess: boolean = await submitForm();
+      await submitForm();
     }
     setState(true);
   };
-  function validateForm(): boolean {
-    console.log("Valide form");
-    return true;
-  }
-  async function submitForm(): Promise<boolean> { // TO DO LINK with form 
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          price: 7954621,
-          title: "Tour Eiffel2",
-          dimensions: "énorme",
-          imagesUrl: "String",
-          description: "Votre propre tour eiffel",
-          gcodeUrl: "blablaurlgcode",
-          creatorId: "pqd1DpVqvIOVjAuAYTzA"
-      })
-    };
-    fetch('https://staythefuckapex-api.azurewebsites.net/api/3dmodels/new', requestOptions)
-    console.log("send");
-    return true;
-  }
 
+  const validateForm = (): boolean => {
+    return true;
+  };
+
+  const submitForm = async () => {
+    let submitted = false;
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        price: 7954621,
+        title: 'Tour Eiffel2',
+        dimensions: 'énorme',
+        imagesUrl: 'String',
+        description: 'Votre propre tour eiffel',
+        gcodeUrl: 'blablaurlgcode',
+        creatorId: 'pqd1DpVqvIOVjAuAYTzA',
+      }),
+    };
+    fetch(
+      'https://staythefuckapex-api.azurewebsites.net/api/3dmodels/new',
+      requestOptions
+    )
+      .then((r) => (submitted = true))
+      .catch((e) => (submitted = false));
+    return submitted;
+  };
 
   return (
-    <Form  onSubmit={handleSubmit} >
+    <Form onSubmit={handleSubmit}>
       <Form.Group controlId='imageUrlField'>
         <Form.Label className='h3 title'>Image URL</Form.Label>
         <Form.Control type='text' placeholder='https://image.url' />
@@ -73,9 +72,7 @@ export const ThreeDModelForm = () => {
       <Button variant='primary' type='submit'>
         Add
       </Button>
-      <div hidden={state}>
-        Loading ...
-      </div>
+      <div hidden={state}>Loading ...</div>
     </Form>
   );
 };
